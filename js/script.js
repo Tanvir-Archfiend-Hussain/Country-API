827b7d0b10982668065f116189f5312e9728a008
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-24 22:12:11 +0000

    recommits

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 
 searchBtn.addEventListener("click", () => {
-    let countryName = 
+  let countryName = countryInp.value;
+  letURL = "";
 });
